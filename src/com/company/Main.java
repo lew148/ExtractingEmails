e787979b5712386996ef1b33e34df921cf3599db
Lewis Reid <lewis.reid148@gmail.com>
2018-09-11 16:41:12 +0100

    Optimised Naming

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,27 +7,30 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
+
+
         String inputtext = "";
+
         try {
             inputtext = Utilities.readFile("C:\\Work\\Training\\ExtractingEmails\\sampletext.txt");
         } catch (IOException e) {
             System.out.println("An error has occurred! File may not exist.");
         }
 
-        //"\W(\w|[.'_%+-])+@softwire\.com\W"
 
-        Pattern pattern = Pattern.compile("\\W(\\w|[.'_%+-])+@softwire\\.com\\W");
-        Matcher matcher = pattern.matcher(inputtext);
+        Pattern emailPattern = Pattern.compile("\\W(\\w|[.'_%+-])+@softwire\\.com\\W");
+        Matcher emailMatcher = emailPattern.matcher(inputtext);
 
         int counter = 0;
 
-        while (matcher.find()) {
+        while (emailMatcher.find()) {
             counter = counter + 1;
 
         }
 
+System.out.println(counter);
+        //@\w*.com|co.uk
 
-        System.out.println(counter);
 
     }
 }
